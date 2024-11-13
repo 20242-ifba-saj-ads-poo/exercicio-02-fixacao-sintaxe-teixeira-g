22d359b39e71088398800044359caf942048df6c
teixeira-g <gabriel.txr00@gmail.com>
2024-11-13 20:01:20 -0300

    Resolução da questão 1

--- src/Questao1.java
+++ src/Questao1.java
@@ -1,5 +1,7 @@
 public class Questao1 {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        for(int i = 150; i < 301; i++){
+            System.out.println(i);
+        }
     }
 }
