17c120ea802eab1a752b097c67b65dce627a6553
teixeira-g <gabriel.txr00@gmail.com>
2024-11-14 09:48:21 -0300

    Resolução da questão 2

--- src/Questao1.java
+++ src/Questao1.java
@@ -1,3 +1,4 @@
+//Imprima todos os números de 150 a 300.
 public class Questao1 {
     public static void main(String[] args) {
         for(int i = 150; i < 301; i++){
---
+++ src/Questao2.java
@@ -0,0 +1,15 @@
+//Imprima a soma de 1 até 1000.
+public class Questao2 {
+    public static void main(String[] args) {
+
+        int soma = 0;
+
+        for (int cont = 1; cont <= 1000; cont++){
+
+           soma +=  cont;
+
+        }
+
+        System.out.println(soma);
+    }
+}
