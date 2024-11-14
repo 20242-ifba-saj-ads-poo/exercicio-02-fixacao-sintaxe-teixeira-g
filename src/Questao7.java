3b567d17a877768fd4d083a0e157e280f65c5d3d
teixeira-g <gabriel.txr00@gmail.com>
2024-11-14 19:39:22 -0300

    Resolução da questão 7

--- src/Questao7.java
+++ src/Questao7.java
@@ -3,8 +3,21 @@
 se x é par, x = x / 2
 se x é impar, x = 3 * x + 1
 imprime x
-O programa deve parar quando x tiver o valor final de 1. Por exemplo, para x = 13, a saída será: 40 -> 20 -> 10 -> 5 -> 16 -> 8 -> 4 -> 2 -> 1*/public class Questao7 {
+O programa deve parar quando x tiver o valor final de 1. Por exemplo, para x = 13, a saída será: 40 -> 20 -> 10 -> 5 -> 16 -> 8 -> 4 -> 2 -> 1*/
+public class Questao7 {
     public static void main(String[] args) {
+
+        int x = 13;
+
+        while (x != 1) {
+            System.out.print(x + " -> ");
+            if (x % 2 == 0) {
+                x = x / 2;
+            } else {
+                x = 3 * x + 1;
+            }
+        }
+        System.out.println(x);
 
     }
 }
